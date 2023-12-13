d21d6f27fa72640ac67a3e5f076c8a1da5bab9d2
Sailor1314 <zhongfeng.wu@edu.sait.ca>
2023-12-13 13:52:23 -0700

    changes

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import MyComp from "./StudentInfo";
 
-export default function Home() {
+export default function Home() 
   return (
     <main>
       <h1 className="text-[50px] font-bold">CPRG 306: Web Development 2 - Assignments</h1>
