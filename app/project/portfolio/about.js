006bdee954a600258166201fb8db7b50fef4b435
Sailor1314 <zhongfeng.wu@edu.sait.ca>
2023-12-14 11:33:03 -0700

    Update about.js

--- app/project/portfolio/about.js
+++ app/project/portfolio/about.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
 const About = () => {
-    return (
-        <div className={styles.container}>
-            <h1>About Me</h1>
-            <p>Hello! I'm Sailor, a passionate CODING based in Calgary.</p>
-            <p>I specialize in React and have 2 years of experience in Software. I love creating things that live on the internet, whether that be websites, applications, or anything in between.</p>
+    // return (
+    //     <div className={styles.container}>
+    //         <h1>About Me</h1>
+    //         <p>Hello! I'm Sailor, a passionate CODING based in Calgary.</p>
+    //         <p>I specialize in React and have 2 years of experience in Software. I love creating things that live on the internet, whether that be websites, applications, or anything in between.</p>
     
-            <h2>Skills</h2>
-            <ul>
-                <li>JavaScript (ES6+)</li>
-                <li>React & Next.js</li>
-                <li>Node.js</li>
-                <li>HTML & CSS</li>
+    //         <h2>Skills</h2>
+    //         <ul>
+    //             <li>JavaScript (ES6+)</li>
+    //             <li>React & Next.js</li>
+    //             <li>Node.js</li>
+    //             <li>HTML & CSS</li>
         
-            </ul>
-        </div>
-    );
+    //         </ul>
+    //     </div>
+    // );
 }
 
 export default About;
